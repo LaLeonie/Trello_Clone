4fc7c352d54920a4e8360041caa3a3fdc0aa488a
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2021-11-10 20:27:32 +0000

    adds styles for input buttons

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,13 +1,15 @@
 import { ColumnContainer, ColumnTitle } from "./styles";
+import { FC } from "react";
 
 type ColumnProps = {
   text: string;
 };
 
-export const Column = ({ text }: ColumnProps) => {
+export const Column: FC<ColumnProps> = ({ text, children }) => {
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
+      {children}
     </ColumnContainer>
   );
 };
