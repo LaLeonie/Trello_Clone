96ce08340fdf400b88d185e6404c4dc5b58045b6
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2021-11-11 11:51:38 +0000

    adds App scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
 import { AppContainer } from "./styles";
+import { AddNewItem } from "./AddNewItem";
+import { Column } from "./Column";
+import { Card } from "./Card";
 
 export function App() {
-  return <AppContainer>App will go here</AppContainer>;
+  return (
+    <AppContainer>
+      <Column text="To Do">
+        <Card text="Generate app scaffold" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn Typescript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typing" />
+      </Column>
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
+    </AppContainer>
+  );
 }
