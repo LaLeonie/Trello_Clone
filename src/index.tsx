deb6f89d3b9a90eda45db9d5f03e922ad3a2cd7c
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2021-11-11 12:46:41 +0000

    adds context component

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { App } from "./App";
+import { AppStateContext } from "./state/AppStateContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppStateContext>
+      <App />
+    </AppStateContext>
   </React.StrictMode>,
   document.getElementById("root")
 );
