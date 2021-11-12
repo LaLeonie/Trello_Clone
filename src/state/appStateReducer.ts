6902f2a11d6b6aaed07d5defd4f6e27b4b542c15
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2021-11-12 10:46:44 +0000

    adds findItemIndeexById helper function

--- src/state/appStateReducer.ts
+++ src/state/appStateReducer.ts
@@ -29,6 +29,12 @@ export const appStateReducer = (
       });
       break;
     }
+    case "ADD_TASK": {
+      const { text, listId } = action.payload;
+      const targetListIndex = findItemIndexById(draft.lists, listId);
+
+      draft.lists[targetListIndex].tasks.push({ id: nanoid(), text });
+    }
     default: {
       return state;
     }
