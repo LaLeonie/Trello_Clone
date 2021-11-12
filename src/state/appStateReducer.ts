424e4667a5edec2a89f6a7a464cfe2c12b08da5a
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2021-11-12 10:35:17 +0000

    modifies reducer to implement use-immer

--- src/state/appStateReducer.ts
+++ src/state/appStateReducer.ts
@@ -16,16 +16,18 @@ export type AppState = {
   lists: List[];
 };
 
-export const appStateReducer = (state: AppState, action: Action): AppState => {
+export const appStateReducer = (
+  draft: AppState,
+  action: Action
+): AppState | void => {
   switch (action.type) {
     case "ADD_LIST": {
-      return {
-        ...state,
-        lists: [
-          ...state.lists,
-          { id: nanoid(), text: action.payload, tasks: [] },
-        ],
-      };
+      draft.lists.push({
+        id: nanoid(),
+        text: action.payload,
+        tasks: [],
+      });
+      break;
     }
     default: {
       return state;
